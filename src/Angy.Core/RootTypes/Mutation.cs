0841e9973617818d9b059fd441be477f62f6bc55
Alberto <aviezzi@outlook.com>
2020-07-03 20:39:53 +0200

    Add updateProduct

--- src/Angy.Core/RootTypes/Mutation.cs
+++ src/Angy.Core/RootTypes/Mutation.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using Angy.Core.Abstract;
+using Angy.Core.Extensions;
 using Angy.Core.Inputs;
+using Angy.Core.Specifications;
 using Angy.Core.Types;
 using Angy.Shared.Model;
 using GraphQL;
 using GraphQL.Types;
 using GraphQL.Utilities;
+using Microsoft.EntityFrameworkCore;
 
 namespace Angy.Core.RootTypes
 {
@@ -40,6 +43,28 @@ namespace Angy.Core.RootTypes
                     await lucifer.CommitAsync();
 
                     return product;
+                });
+
+            FieldAsync<ProductType>(
+                "updateProduct",
+                arguments: new QueryArguments(
+                    new QueryArgument<StringGraphType> {Name = "id"},
+                    new QueryArgument<NonNullGraphType<ProductInputType>> {Name = "product"}
+                ),
+                resolve: async context =>
+                {
+                    var lucifer = _provider.GetRequiredService<ILuciferContext>();
+
+                    var id = context.GetArgument<Guid>("id");
+                    var product = context.GetArgument<Product>("product");
+
+                    var entity = await lucifer.Products.Specify(new ProductIdSpecification(id)).FirstOrDefaultAsync();
+                    entity.Name = product.Name;
+                    entity.Description = product.Description;
+
+                    await lucifer.CommitAsync();
+
+                    return entity;
                 });
         }
     }
