7ec2d412be37cf925496604f97ac3919e52d65c0
Alberto <aviezzi@outlook.com>
2020-07-10 22:57:58 +0200

    Refactoring

--- src/Angy.BackEndClient/FormValidators/SelectValidator.cs
+++ src/Angy.BackEndClient/FormValidators/SelectValidator.cs
@@ -10,10 +10,7 @@
         {
             var result = Guid.TryParse(value.ToString(), out var guid);
 
-            if (result && guid != Guid.Empty)
-            {
-                return null;
-            }
+            if (result && guid != Guid.Empty) return null;
 
             return new ValidationResult("Select a option.",
                 new[] { validationContext.MemberName });
